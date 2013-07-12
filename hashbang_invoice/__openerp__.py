759a1ae5f1075a7094a0d9d10042b302888fe708
Arthur Vuillard <arthur.vuillard@gmail.com>
2013-07-12 22:21:03 +0200

    Make hashbang_invoice a real openerp module

---
+++ hashbang_invoice/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'HashBang invoice report',
+    'version': '1',
+    'category': 'Reports/Webkit',
+    'description': """
+Replaces the default invoice webkit report by a more suitable one for Hashbang SARL.
+""" ,
+    'author': 'Arthur Vuillard',
+    'website': 'http://hashbang.fr',
+    'depends': ['invoice_webkit'],
+    'data': ['data/base_headers_data.xml', 'data/invoice_report.xml'],
+    'demo_xml': [],
+    'test': [],
+    'installable': True,
+    'active': False,
+}
+
